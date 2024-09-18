00d761ac9cf4a1d2cb832d266cb97182430ae240
Akash kafle <aakashkafle47@gmail.com>
2024-09-18 17:22:58 +0545

    added algo

--- backend/fraud_buster/algorithms/algo.py
+++ backend/fraud_buster/algorithms/algo.py
@@ -1,16 +1,35 @@
 # all the algorithms are implemented here 
-
-#Isolation forest 
 from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import train_test_split
-
-# Preprocess data (scaling, feature extraction)
-X_train, X_test = train_test_split(data, test_size=0.2)
-isolation_forest = IsolationForest(contamination=0.01)
-isolation_forest.fit(X_train)
-
-# Prediction
-y_pred = isolation_forest.predict(X_test)
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from hmmlearn import hmm
+from sklearn.neighbors import KNeighborsClassifier
 
 
-#Support Vector Machine (SVM) with time-series analysis
+def train_isolation_forest(data):
+    X_train, X_test = train_test_split(data, test_size=0.2)
+    isolation_forest = IsolationForest(contamination=0.01)
+    isolation_forest.fit(X_train)
+    y_pred = isolation_forest.predict(X_test)
+    return y_pred
+
+def train_svm(X_train, y_train, X_test):
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    svm_model = SVC(kernel='linear')
+    svm_model.fit(X_train_scaled, y_train)
+    y_pred = svm_model.predict(X_test)
+    return y_pred
+
+def train_hmm(data, X_test):
+    hmm_model = hmm.GaussianHMM(n_components=3)
+    hmm_model.fit(data)
+    hidden_states = hmm_model.predict(X_test)
+    return hidden_states
+
+def train_knn(X_train, y_train, X_test):
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    return y_pred
